7aa80522fa59532c63bf144be0d9898b4507ce65
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-05-17 18:29:48 -0700

    step04_fig4_blue_green example

---
+++ ch03-usmap/step04_fig4_blue_green/sketch.js
@@ -0,0 +1,60 @@
+let mapImage;
+let locationTable;
+let rowCount;
+
+let dataTable;
+let dataMin = Number.MAX_VALUE;
+let dataMax = Number.MIN_VALUE;
+
+// Any files, data or graphic, that need to be used immediately should
+// be loaded in preload, which ensure the resource is full loaded and
+// available before any code in setup is run.
+function preload() {
+  mapImage = loadImage("data/map.png");
+  locationTable = loadTable("data/locations.tsv");
+
+  // Read the data table
+  dataTable = new loadTable("data/random.tsv");
+}
+
+
+function setup() {
+  createCanvas(640, 400);
+  rowCount = locationTable.getRowCount();
+
+  // Find the minimum and maximum values
+  for (let row = 0; row < rowCount; row++) {
+    let v = dataTable.getNum(row, 1);
+    if (v > dataMax) {
+      dataMax = v;
+    }
+    if (v < dataMin) {
+      dataMin = v;
+    }
+  }
+  smooth();
+  noStroke();
+}
+
+
+function draw() {
+  background(255);
+  tint(255, 160);
+  image(mapImage, 0, 0);
+
+  for (let row = 0; row < rowCount; row++) {
+    let abbrev = dataTable.getString(row, 0);
+    let x = locationTable.findRow(abbrev, 0).getString(1)
+    let y = locationTable.findRow(abbrev, 0).getString(2)
+    drawData(x, y, abbrev);
+  }
+}
+
+
+function drawData(x, y, abbrev) {
+  let v = dataTable.findRow(abbrev, 0).getString(1);
+  let percent = norm(v, dataMin, dataMax);
+  between = lerpColor('#296F34', '#61E2F0', percent);
+  fill(between);
+  ellipse(x, y, 15, 15);
+}
