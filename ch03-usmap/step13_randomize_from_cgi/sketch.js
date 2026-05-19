47f30267ef5641e412f9292db283a32535de3a73
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-05-18 18:01:14 -0700

    step13_randomize_from_cgi example

---
+++ ch03-usmap/step13_randomize_from_cgi/sketch.js
@@ -0,0 +1,127 @@
+let mapImage;
+let locationTable;
+let nameTable;
+let rowCount;
+
+let dataTable;
+let dataMin = -10;
+let dataMax = 10;
+let font;
+
+let loading, loaded;
+let randomCount = 1;
+
+// Any files, data or graphic, that need to be used immediately should
+// be loaded in preload, which ensure the resource is full loaded and
+// available before any code in setup is run.
+function preload() {
+  mapImage = loadImage("data/map.png");
+  locationTable = loadTable("data/locations.tsv");
+  nameTable = loadTable("data/names.tsv");
+
+  // Read the data table
+  dataTable = new loadTable("data/random.tsv");
+
+  font = loadFont("data/UniversalisADFStd-Bold.otf");
+}
+
+
+function setup() {
+  createCanvas(640, 400);
+  rowCount = locationTable.getRowCount();
+
+  smooth();
+  noStroke();
+  textFont(font);
+}
+
+
+// Global variables set in drawData() and read in draw()
+let closestDist;
+let closestText;
+let closestTextX;
+let closestTextY;
+
+
+function draw() {
+  background(255);
+  image(mapImage, 0, 0);
+
+  if (!loading && loaded && loadingTable !== null) {
+    dataTable = loadingTable;
+    loadingTable = null;
+    loaded = false;
+  }
+
+  // Use the built-in width and height variables to set the
+  // closest distance high so it will be replaced immediately
+  closestDist = width * height;
+
+  for (let row = 0; row < rowCount; row++) {
+    let abbrev = dataTable.getString(row, 0);
+    let x = locationTable.findRow(abbrev, 0).getNum(1)
+    let y = locationTable.findRow(abbrev, 0).getNum(2)
+    drawData(x, y, abbrev);
+  }
+
+  // Use global variables set in drawData()
+  // to draw text related to closest circle.
+  if (closestDist != width * height) {
+    fill(0);
+    textAlign(CENTER);
+    text(closestText, closestTextX, closestTextY);
+  }
+}
+
+
+function drawData(x, y, abbrev) {
+  let v = dataTable.findRow(abbrev, 0).getString(1);
+  let radius = 0;
+  if (v >= 0) {
+    radius = map(v, 0, dataMax, 1.5, 15);
+    fill('#333366');  // blue
+  } else {
+    radius = map(v, 0, dataMin, 1.5, 15);
+    fill('#ec5166');  // red
+  }
+  ellipseMode('RADIUS');
+  ellipse(x, y, radius, radius);
+
+  let d = dist(x, y, mouseX, mouseY);
+  if ((d < radius + 2) && (d < closestDist)) {
+    closestDist = d;
+    let name = nameTable.findRow(abbrev, 0).getString(1);
+    closestText = name + " " + nfp(v, 0, 2);
+    closestTextX = x;
+    closestTextY = y - radius - 4;
+  }
+}
+
+
+function keyPressed() {
+  if (key == ' ') {
+    updateTable();
+  }
+}
+
+
+function updateTable() {
+  loading = true;
+
+  if (randomCount < 5) {
+    randomCount++;
+  } else {
+    randomCount = 1;
+  }
+
+  // Note, that this simulates making an asyncronous call to get data,
+  // but the endpoint is different from Fry's Visualizing Data
+  // (instead local files are used), because CORS permisions don't
+  // allow http://benfry.com/writing/map/random.cgi to be loaded from
+  // javascript.
+  loadTable("data/random0" + String(randomCount) + ".tsv", "tsv", t => {
+    loadingTable = t;
+    loaded = true;
+    loading = false;
+  });
+}
