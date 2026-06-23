f23dbccb1735706c1afac36847ff112794a36110
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-06-23 15:42:45 -0700

    step_08b_web -- upload team gifs

--- ch05-salaryper/step_08b_web/sketch.js
+++ ch05-salaryper/step_08b_web/sketch.js
@@ -197,6 +197,7 @@ function preload() {
   }
 
 
+
   // Standings information
   standingsFor240601 =
     loadJSON("https://statsapi.mlb.com/api/v1/standings?leagueId=103&season=2024&date=2024-06-01");
