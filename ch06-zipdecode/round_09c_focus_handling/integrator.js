eab0d0f516760784fdbae74da22ed7704aad1f38
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-08-16 19:58:52 -0700

    round_09c_focus_handling

---
+++ ch06-zipdecode/round_09c_focus_handling/integrator.js
@@ -0,0 +1,59 @@
+class Integrator {
+
+  constructor(value = 100, damping = 0.5, attraction = 0.2) {
+    this.force = 0;
+    this.vel = 0;
+    this.accel = 0;
+    this.mass = 1;
+
+    this.damping = damping;
+    this.attraction = attraction;
+
+    this._value = value;
+    this._target = value;
+
+    this.prev = Number.MAX_VALUE;
+    this.epsilon = 0.0001;
+  }
+
+  value(v) {
+    if (typeof v !== 'undefined') {
+      this._value = v;
+      return this;
+    }
+    return this._value;
+  }
+
+  update() {
+    if (this.targeting) {
+      this.force += this.attraction * (this._target - this._value);
+    }
+
+    this.accel = this.force / this.mass;
+    this.vel = (this.vel + this.accel) * this.damping;
+    this._value += this.vel;
+
+    this.force = 0;
+
+    if (Math.abs(this._value - this.prev) < this.epsilon) {
+      this._value = this._target;
+      return false;
+    }
+
+    this.prev = this._value;
+    return true;
+  }
+
+  target(t) {
+    if (typeof t !== 'undefined') {
+      this.targeting = true;
+      this._target = t;
+      return this;
+    }
+    return this._target;
+  }
+
+  noTarget() {
+    this.targeting = false;
+  }
+}
