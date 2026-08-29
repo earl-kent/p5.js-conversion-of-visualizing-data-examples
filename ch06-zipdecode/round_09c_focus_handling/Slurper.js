68f6529372d04e0572a6b0b72b24358e95222262
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-08-28 20:51:07 -0700

    round_09c_focus_handling

--- ch06-zipdecode/round_09c_focus_handling/Slurper.js
+++ ch06-zipdecode/round_09c_focus_handling/Slurper.js
@@ -1,18 +1,13 @@
 // Code from Visualizing Data, First Edition, Copyright 2008 Ben Fry.
 
 
-class Slurper implements Runnable {
-
-  Slurper() {
-    Thread thread = new Thread(this);
-    thread.start();
-  }
+class Slurper {
 
   public void run() {
     try {
       InputStream input = openStream("zips.gz");
       BufferedReader reader = createReader(input);
-      
+
       // first get the info line
       String line = reader.readLine();
       parseInfo(line);
